edc568e26678aa0a239b87f054e236a1637af2bf
Matthew Brost <matthew.brost@intel.com>
2023-10-30 20:24:35 -0700

    drm/sched: Add drm_sched_wqueue_* helpers
    Add scheduler wqueue ready, stop, and start helpers to hide the
    implementation details of the scheduler from the drivers.
    
    v2:
      - s/sched_wqueue/sched_wqueue (Luben)
      - Remove the extra white line after the return-statement (Luben)
      - update drm_sched_wqueue_ready comment (Luben)
    
    Cc: Luben Tuikov <luben.tuikov@amd.com>
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: Luben Tuikov <luben.tuikov@amd.com>
    Link: https://lore.kernel.org/r/20231031032439.1558703-2-matthew.brost@intel.com
    Signed-off-by: Luben Tuikov <ltuikov89@gmail.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4605,7 +4605,7 @@ bool amdgpu_device_has_job_running(struct amdgpu_device *adev)
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !ring->sched.thread)
+		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 			continue;
 
 		spin_lock(&ring->sched.job_list_lock);
@@ -4732,7 +4732,7 @@ int amdgpu_device_pre_asic_reset(struct amdgpu_device *adev,
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !ring->sched.thread)
+		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 			continue;
 
 		/*clear job fence from fence drv to avoid force_completion
@@ -5271,7 +5271,7 @@ int amdgpu_device_gpu_recover(struct amdgpu_device *adev,
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = tmp_adev->rings[i];
 
-			if (!ring || !ring->sched.thread)
+			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 				continue;
 
 			drm_sched_stop(&ring->sched, job ? &job->base : NULL);
@@ -5345,7 +5345,7 @@ skip_hw_reset:
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = tmp_adev->rings[i];
 
-			if (!ring || !ring->sched.thread)
+			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 				continue;
 
 			/* No point to resubmit jobs if we didn't HW reset*/
@@ -5674,7 +5674,7 @@ pci_ers_result_t amdgpu_pci_error_detected(struct pci_dev *pdev, pci_channel_sta
 		for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 			struct amdgpu_ring *ring = adev->rings[i];
 
-			if (!ring || !ring->sched.thread)
+			if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 				continue;
 
 			drm_sched_stop(&ring->sched, NULL);
@@ -5802,7 +5802,7 @@ void amdgpu_pci_resume(struct pci_dev *pdev)
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
 		struct amdgpu_ring *ring = adev->rings[i];
 
-		if (!ring || !ring->sched.thread)
+		if (!ring || !drm_sched_wqueue_ready(&ring->sched))
 			continue;
 
 
